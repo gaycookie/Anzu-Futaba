ff51b8dfbebc7946d460cb277e10c37f318c7e44
Kawaii Programming <34284353+kawaaii@users.noreply.github.com>
2018-06-09 19:27:05 +0200

    Added: eval command (simple).
    This command can only be used by Bot owners, from the config.json file.

---
+++ commands/eval.js
@@ -0,0 +1,36 @@
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
+
+module.exports = {
+    name: 'eval',
+    description: 'Eval stuff',
+    args: true,
+    guildOnly: false,
+    ownerOnly: true,
+    execute(message, args) {
+
+        const clean = text => {
+            if (typeof(text) === "string")
+              return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+            else
+                return text;
+        }
+
+        try {
+            const code = args.join(" ");
+            let evaled = eval(code);
+      
+            if (typeof evaled !== "string")
+              evaled = require("util").inspect(evaled);
+      
+            message.channel.send(clean(evaled), {code:"xl"});
+        } catch (err) {
+            message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        }
+
+    },
+};
