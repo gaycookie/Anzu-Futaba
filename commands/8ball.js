e4a7ec79221df03df2ef83197039973d0cd606f4
Kawaii Programming <34284353+kawaaii@users.noreply.github.com>
2018-06-09 19:24:42 +0200

    Added: 8ball command.

---
+++ commands/8ball.js
@@ -0,0 +1,35 @@
+const answers = require('../data/eightball_answers.json')
+
+module.exports = {
+    name: '8ball',
+    description: `Ask the :crystal_ball: a question and get an answer.`,
+    usage: '[question]',
+    //args: true,
+    guildOnly: true,
+    //guildOwnerOnly: true,
+    //permissions: 'manage_guild',
+    //disabled: true,
+    //ownerOnly: true,
+    execute(message, args) {
+
+        if (!args.length) return message.channel.send(`${message.author}, you need to ask me something, b-baka!`);
+
+        var answer = answers.answers[Math.floor(Math.random()*answers.answers.length)];
+        const embed = {
+            "color": 16670894,
+            "thumbnail": {
+                "url": `${message.author.avatarURL}`
+            },
+            "fields": [{
+                name: `${message.author.username} asks: `,
+                value: `${args.join(" ")}`
+            },
+            {
+                name: `${message.client.user.username} answers: `,
+                value: `${answer}`
+            }],
+        }
+        message.channel.send({embed})
+
+    },
+};
