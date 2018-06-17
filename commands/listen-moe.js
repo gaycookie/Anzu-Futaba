05b0a14670d4d4e0c6b1bc358acebf0292a94248
Kawaii Programming <34284353+kawaaii@users.noreply.github.com>
2018-06-17 06:32:05 +0200

    Edited: listen-moe

--- commands/listen-moe.js
+++ commands/listen-moe.js
@@ -1,21 +1,24 @@
 module.exports = {
     name: 'listen',
     description: `Wanna listen to LISTEN.moe radio? Join a voice channel, and then run this command.`,
-    //usage: '[emoji]',
     aliases: ['listenmoe', 'join'],
     permissions: 'manage_guild',
     guildOnly: true,
     execute(message, args) {
 
-        let guilds;
         const { voiceChannel } = message.member;
+        const { voiceConnection } = message.guild;
 
-        if (message.guild.voiceConnection) {
-            return message.channel.send('I am already in a voice channel here, b-baka! ｡゜(｀Д´)゜｡');
+        if (voiceConnection) {
+            voiceConnection.disconnect();
         }
 
         if (!voiceChannel) {
             return message.channel.send('you have to be in a voice channel to add me, b-baka! ｡゜(｀Д´)゜｡');
+        }
+
+        if (!voiceChannel.joinable) {
+            return message.channel.send('I can not join this voice channel, b-baka! ｡゜(｀Д´)゜｡');
         }
 
         voiceChannel.join({ shared: true })
