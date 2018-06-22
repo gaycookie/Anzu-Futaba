db201b78cd54e61e15bee7e0784366fb9d0c4505
Kawaii Programming <34284353+kawaaii@users.noreply.github.com>
2018-06-22 09:45:14 +0200

    Fixed `handleDisconnect()`

--- db.js
+++ db.js
@@ -1,16 +1,38 @@
 const config    = require('./data/config.json');
 const mysql     = require('mysql');
-var connection  = mysql.createConnection({
+
+const mysqlConfig = {
     connectionLimit : config.database['connectionLimit'],
     host     : config.database['host'],
     user     : config.database['user'],
     password : config.database['pass'],
     database : config.database['db'],
     charset  : config.database['charset'],
-});
+    dateStrings: true
+}
 
-connection.connect(function(err) {
-    if (err) throw err;
-});
+var connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(mysqlConfig);
+  
+    connection.connect(function(err) {
+        if(err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+  
+handleDisconnect();
 
 module.exports = connection;
