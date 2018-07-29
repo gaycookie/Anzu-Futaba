800e8069e853e39030e31a191c00ebb5e8b4c9b6
Kawaii Programming <34284353+kawaaii@users.noreply.github.com>
2018-07-29 13:13:08 +0200

    Hmmm

---
+++ custom_modules/Listen-moe.js
@@ -0,0 +1,44 @@
+const Discord           = require('discord.js');
+const streamKPOP        = 'async:https://listen.moe/kpop/opus';
+const streamJPOP        = 'async:https://listen.moe/opus';
+const streamOptions     = { passes: 10, bitrate: 'auto' }
+
+const main              = require('../main.js');
+const settings          = main.settings;
+
+function speaking(client, connection, dispatcher) {
+    const hook = new Discord.WebhookClient('472727454054744069', 'hhaicowySuB5qAxGvwcO9rHt3OFl1SzmAf1Ij1wA3BD3cFTX7A4TlUwhFASpdwXCP96u');
+    dispatcher.on('speaking', (speaking) => { // Weird that User was not a thing.. I guess the Doc's aren't up to date!
+        console.log(speaking);
+        if (speaking == false) {
+            hook.send(`${connection.client.users.get('139191103625625600')} | Something went wrong with the stream in "${connection.channel.name}"`);
+        };
+    })
+}
+
+function auto_play(client) {
+    for (let item of settings.getAllKPOP()) {
+        if (client.channels.get(item).type === 'voice') {
+            client.channels.get(item).join({ shared: true })
+            .then(connection => {
+                const dispatcher = connection.playStream(streamKPOP, streamOptions);
+                speaking(client, connection, dispatcher);
+            })
+            .catch(vc => { })
+        };
+    };
+
+    for (let item of settings.getAllJPOP()) {
+        if (client.channels.get(item).type === 'voice') {
+            client.channels.get(item).join({ shared: true })
+            .then(connection => {
+                const dispatcher = connection.playStream(streamJPOP, streamOptions);
+                speaking(client, connection, dispatcher);
+            })
+            .catch(vc => { })
+        };
+    };
+}
+
+module.exports.auto_play        = auto_play;
+module.exports.playing_check    = speaking;
--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,17 @@
-const GuildSettings     = require('../guildSettings.js');
-const settings          = GuildSettings.lastSettings();
-const auto_track        = require('../custom_modules/current-track.js')
+const auto_track        = require('../custom_modules/current-track.js');
 const main              = require('../main.js');
+const settings          = main.settings;
+
+
 //const moe               = main.moe;
 //const feeder            = main.feeder;
-const humble_bundle     = require('../custom_modules/humble-bundle.js');
-const Discord           = require('discord.js');
+//const humble_bundle     = require('../custom_modules/humble-bundle.js');
+//const Discord           = require('discord.js');
 
 exports.run = (client) => {
+
+    const listen_moe        = require('../custom_modules/Listen-moe.js');
+    listen_moe.auto_play(client);
 
     //moe.on('updateTrack', (current_track) => {
     //    channel = client.channels.get('458310090454466562');
@@ -41,44 +45,6 @@ exports.run = (client) => {
             client.user.setActivity(`${status[current]}`, { url: 'https://www.twitch.tv/noella_kawaii', type: 'STREAMING' })
             await sleep(20000);
             current++;
-        }
-    }
-
-    function speaking(connection, dispatcher) {
-        const hook = new Discord.WebhookClient('472727454054744069', 'hhaicowySuB5qAxGvwcO9rHt3OFl1SzmAf1Ij1wA3BD3cFTX7A4TlUwhFASpdwXCP96u');
-        dispatcher.on('speaking', (speaking) => { // Weird that User was not a thing.. I guess the Doc's aren't up to date!
-            console.log(speaking);
-            if (speaking == true) {
-                hook.send(`${connection.client.users.get('139191103625625600')} | Started Streaming in "${connection.channel.name}"`)
-            } else {
-                hook.send(`${connection.client.users.get('139191103625625600')} | Something went wrong with the stream in "${connection.channel.name}"`)
-            }
-        })
-    }
-
-    const streamKPOP = 'async:https://listen.moe/kpop/opus';
-    const streamJPOP = 'async:https://listen.moe/opus';
-    const streamOptions = { passes: 10, bitrate: 'auto' }
-
-    for (let item of settings.getAllKPOP()) {
-        if (client.channels.get(item).type === 'voice') {
-            client.channels.get(item).join({ shared: true })
-                .then(connection => {
-                    const dispatcher = connection.playStream(streamKPOP, streamOptions);
-                    speaking(connection, dispatcher);
-                })
-                .catch(vc => { })
-        }
-    }
-
-    for (let item of settings.getAllJPOP()) {
-        if (client.channels.get(item).type === 'voice') {
-            client.channels.get(item).join({ shared: true })
-                .then(connection => {
-                    const dispatcher = connection.playStream(streamJPOP, streamOptions);
-                    speaking(connection, dispatcher);
-                })
-                .catch(vc => { })
         }
     }
 
